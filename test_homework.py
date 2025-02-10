14deee7995aa1e2d73d2d93fd70b97d94ac25991
Ilia <kalinin499studing@gmail.com>
2025-02-10 13:51:42 +0300

    added function implementations

--- test_homework.py
+++ test_homework.py
@@ -1,5 +1,3 @@
-
-
 def test_greeting():
     """
     Напишите программу, которая выводит на экран приветствие.
@@ -7,11 +5,9 @@ def test_greeting():
     name = "Анна"
     age = 25
     # TODO Сформируйте нужную строку
-    output = ""
-
+    output = f"Привет, {name}! Тебе {age} лет."
     # Проверяем результат
     assert output == "Привет, Анна! Тебе 25 лет."
-
 
 def test_rectangle():
     """
@@ -21,43 +17,38 @@ def test_rectangle():
     a = 10
     b = 20
     # TODO сосчитайте периметр
-    perimeter = 0
-
+    perimeter = (a + b) * 2
     assert perimeter == 60
 
     # TODO сосчитайте площадь
-    area = 0
-
+    area = a * b
     assert area == 200
-
 
 def test_circle():
     """
     Напишите программу, которая берет радиус круга и выводит на экран его длину и площадь.
     Используйте константу PI
     """
+    from  math import pi
     r = 23
     # TODO сосчитайте площадь
-    area = 0
-
+    area = pi * r ** 2
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
-    length = 0
-
+    length = 2 * pi * r
     assert length == 144.51326206513048
-
 
 def test_random_list():
     """
     Создайте список из 10 случайных чисел от 1 до 100 (включая обе границы) и отсортируйте его по возрастанию.
     """
     # TODO создайте список
-    l = []
-
+    from random import randint
+    l = [randint(1, 101) for i in range(10)]
+    l.sort()
     assert len(l) == 10
     assert all(l[i] <= l[i + 1] for i in range(len(l) - 1))
-
 
 def test_unique_elements():
     """
@@ -65,11 +56,10 @@ def test_unique_elements():
     """
     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
     # TODO удалите повторяющиеся элементы
-
+    l = list(set(l))
     assert isinstance(l, list)
     assert len(l) == 10
     assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
 
 def test_dicts():
     """
@@ -80,8 +70,7 @@ def test_dicts():
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
-    d = {}
-
+    d = dict(zip(first, second))
     assert isinstance(d, dict)
     assert len(d) == 5
     assert list(d.keys()) == first
